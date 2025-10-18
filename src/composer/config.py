3534498bdd932bd199a9e038d44d02d8c52c7cc3
Joel Nicholas <144729619+jnicholas25@users.noreply.github.com>
2025-10-18 18:56:35 -0500

    Add default configuration settings for composer

---
+++ src/composer/config.py
@@ -0,0 +1,9 @@
+DEFAULTS = {
+    "bars_major": 12,
+    "bars_minor": 12,
+    "tempo_major": 92,
+    "tempo_minor": 82,
+    "ngram_order": 2,
+    "quantize_divs": 4,   # 4 = 16th notes per quarter
+    "tonic_midi": 60,     # C4
+}
