aae906475fb43ae8cb480e870561d99bf9bfa813
jnicholas25 <144729619+jnicholas25@users.noreply.github.com>
2025-10-18 20:26:19 -0500

    code changes and setting the right interpreter

--- src/composer/transcription.py
+++ src/composer/transcription.py
@@ -4,5 +4,12 @@ from basic_pitch import ICASSP_2022_MODEL_PATH
 
 def mp3toMIDI(audio_path: str, out_dir: str) -> list[str]:
   output = Path(out_dir)
-  out.mkdir(exist_ok=True)
+  output.mkdir(exist_ok=True)
 
+  predict(
+    [audio_path],
+    save_midi=True,
+    save_model_outputs=False,     # skip raw model dumps 
+        model_or_model_path=ICASSP_2022_MODEL_PATH,  # official pretrained model
+        output_directory=str(output)     # where to write results
+  )
